0ace9fb5f114c833c63a086c9fbb0f2ef3895efd
kodali <kodali444@gmail.com>
2021-09-23 02:24:04 +0530

    feat(booksApiDesign): updating book api added

--- routes/books.js
+++ routes/books.js
@@ -62,6 +62,70 @@ const bookRoutes = (app, fs) => {
       }
     }, true);
   });
+
+  //to update existing record
+  app.put("/books", (req, res) => {
+    readFile((data) => {
+      const { original_book, new_book, ...remainData } = req?.body?.info;
+      console.log("ORIGINAL", original_book, "NEW", new_book);
+
+      if (
+        original_book != undefined &&
+        original_book?.trim() != "" &&
+        new_book != undefined &&
+        new_book?.trim() != ""
+      ) {
+        if (
+          original_book?.trim().localeCompare(new_book?.trim()) === 0 ||
+          data.books.includes(new_book?.trim())
+        ) {
+          res.status(400).send({
+            status: 400,
+            message:
+              "Invalid pay load. new_book should be different and it should not be other book name which is alreay existed",
+          });
+        } else {
+          let bookIndex = data.books.indexOf(original_book);
+          if (bookIndex != -1) {
+            data.books[bookIndex] = new_book;
+            writeFile(JSON.stringify(data, null, 2), () => {
+              res
+                .status(200)
+                .send({ status: 200, message: "book name is updated" });
+            });
+          }
+        }
+      } else {
+        res.status(400).send({
+          status: 400,
+          message: "Invalid pay load.original_book, new_book are mandatory",
+        });
+      }
+      // if (data[userId] === undefined || data[userId] === "undefined") {
+      //   res.status(404).send(`user id:${userId} not existed`);
+      // } else {
+      //   data[userId] = req.body;
+      //   writeFile(JSON.stringify(data, null, 2), () => {
+      //     res.status(200).send(`users id:${userId} updated`);
+      //   });
+      // }
+    }, true);
+  });
+
+  // to delete existing record
+  app.delete("/users/:id", (req, res) => {
+    readFile((data) => {
+      const userId = req.params["id"];
+      if (data[userId] === undefined || data[userId] === "undefined") {
+        res.status(404).send(`users id:${userId} not existed`);
+      } else {
+        delete data[userId];
+        writeFile(JSON.stringify(data, null, 2), () => {
+          res.status(200).send(`users id:${userId} removed`);
+        });
+      }
+    }, true);
+  });
 };
 
 module.exports = bookRoutes;
