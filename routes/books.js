79a244218640ae94e68a5251c441763694d10876
kodali <kodali444@gmail.com>
2021-09-23 00:35:15 +0530

    feat(booksapidesign): adding book to list api designed

--- routes/books.js
+++ routes/books.js
@@ -32,7 +32,34 @@ const bookRoutes = (app, fs) => {
   // to get list of books
   app.get("/getBookList", (req, res) => {
     readFile((data) => {
-      res.send(data?.books?.join("~"));
+      res.status(200).send({
+        namesList: data?.books?.join("~"),
+        message: "Books string with ~ seperator",
+      });
+    }, true);
+  });
+
+  // to add book to library
+  app.post("/books", (req, res) => {
+    readFile((data) => {
+      let isExisted = false;
+
+      if (req?.body?.book != undefined && req?.body?.book.trim() != "") {
+        if (data.books.includes(req?.body?.book.trim())) {
+          res
+            .status(409)
+            .send({ status: 409, message: "book name already added" });
+        } else {
+          data.books.push(req.body.book);
+          writeFile(JSON.stringify(data, null, 2), () => {
+            res
+              .status(200)
+              .send({ status: 200, message: "Book added successfully" });
+          });
+        }
+      } else {
+        res.status(404).send({ status: 404, message: "Invalid payload" });
+      }
     }, true);
   });
 };
